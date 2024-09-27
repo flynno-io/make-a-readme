d04c3047648855305aaa25a4668c09850c766c05
flynno <luke.laz.flynn@gmail.com>
2024-09-27 13:05:55 -0600

    added licenseChoices.js for import

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// TODO: Include packages needed for this application
+// /index.js
 import inquirer from "inquirer";
 import generateMarkdown from "./utils/generateMarkdown";
+import licenseChoices from "./utils/licenseChoices";
+
 
 // TODO: Create an array of questions for user input
 const questions = [
-    // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
     {
         name: "title",
         type: "input",
@@ -29,23 +30,7 @@ const questions = [
         name: "license",
         type: "rawlist",
         message: "What license do you want to use? ",
-        choices: [
-            "Affero General Public (AGPL)",
-            "Apache 2.0",
-            "BSD (3-Clause, 2-Clause)",
-            "Boost Software License 1.0",
-            "Common Development and Distribution (CDDL)",
-            "Creative Commons Zero (CC0)",
-            "Dual Licensing",
-            "Eclipse Public (EPL)",
-            "GNU General Public (GPL) 2.0 & 3.0",
-            "GNU Lesser General Public (LGPL)",
-            "Microsoft Public (Ms-PL)",
-            "MIT",
-            "Mozilla Public (MPL)",
-            "Proprietary",
-            "Unlicense",
-        ]
+        choices: licenseChoices,
     },
     {
         type: "editor",
@@ -95,27 +80,4 @@ function init() {
 
 // Function call to initialize app
 init();
-
-// *** GIVEN a command-line application that accepts user input ***
-
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
 
